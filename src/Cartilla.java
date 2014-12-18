91b85b5f00362e904fc21d1626e827f141ada599
ghakan <nohtrim@gmail.com>
2014-12-18 09:25:22 +0100

    fds

--- src/Cartilla.java
+++ src/Cartilla.java
@@ -3,7 +3,11 @@ private boolean[] sellos; //array de 10, para guardar los sellos, cada, item ser
 
 //metodo sellar
 	public void setSellos(boolean[] dato){
-		this.sellos = dato;
+		/*Cuando se llega hay 6 sellos, los siguientes pintxo potes, 
+		tienen un descuento del %25. Cuando lleguen a 8 sellos, 
+		los siguientes pintxo potes tienes un descuento del %50. 
+		Si completan toda la cartilla, los siguientes pintxos gratis.*/
+		this.sellos = dato; 
 	}
 //metodo devolver dto correspondiente
 	public boolean[] getSellos(){
